76144bd50bd5429020f3ad2a24a7b05f0a48de7c
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-08-23 15:08:48 +0700

    GuiceServletContextListener: add convenience methods, improve docs, logs

--- src/main/java/pl/morgwai/base/servlet/scopes/GuiceServletContextListener.java
+++ src/main/java/pl/morgwai/base/servlet/scopes/GuiceServletContextListener.java
@@ -3,6 +3,7 @@ package pl.morgwai.base.servlet.scopes;
 
 import java.util.EnumSet;
 import java.util.LinkedList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.DispatcherType;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.websocket.DeploymentException;
 import javax.websocket.server.ServerContainer;
 import javax.websocket.server.ServerEndpointConfig;
+import javax.websocket.server.ServerEndpointConfig.Configurator;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -26,39 +28,40 @@ import com.google.inject.Module;
 
 
 /**
- * Creates and configures app wide Guice <code>Injector</code> and {@link ServletModule}.
+ * Creates and configures app wide Guice {@link #INJECTOR} and {@link ServletModule}.
  * A single subclass of this class must be created and either annotated with
- * <code>@WebListener</code> or enlisted in <code>web.xml</code> file in <code>listener</code>
- * element.
+ * &commat;{@link javax.servlet.annotation.WebListener WebListener} or enlisted in
+ * <code>web.xml</code> file in <code>listener</code> element.
  */
 public abstract class GuiceServletContextListener implements ServletContextListener {
 
 
 
 	/**
-	 * @return Guice <code>Module</code> list that configure bindings for all user-defined
-	 * components that will be requested from Guice {@link #INJECTOR}. This method may use
-	 * <code>Scope</code>s and <Code>Tracker</code>s and helper methods from
-	 * {@link #servletModule}.
+	 * @return Guice {@link Module} list that configure bindings for all user-defined components
+	 * that will be requested from Guice {@link #INJECTOR}. This method may use <code>Scope</code>s
+	 * and <Code>Tracker</code>s and helper methods from {@link #servletModule}.
 	 */
 	protected abstract LinkedList<Module> configureInjections() throws ServletException;
-	protected ServletModule servletModule;
+	protected ServletModule servletModule = new ServletModule();
 
 	/**
-	 * App wide Guice <code>Injector</code>. It's made public static so that non-programmatic
-	 * servlets/filters that cannot extend GuicifiedServlet/GuicifiedFilter may use it directly
-	 * to inject their dependencies (usually in the <code>init(config)</code> method).
+	 * App wide Guice {@link Injector}. Exposed as {@code public static} for non-programmatic
+	 * servlets/filters to manually request dependency injection with
+	 * {@link Injector#injectMembers(Object)} (usually in the <code>init(config)</code> method).
 	 */
 	public static Injector INJECTOR;
 
 
 
 	/**
-	 * Programmatically adds servlets, filters and endpoints. If all of these components are
-	 * configured via annotations or <code>web.xml</code> file, then this method can be left empty.
+	 * Programmatically adds servlets, filters and endpoints.
+	 * <p>
+	 * If all of these components are configured via annotations or <code>web.xml</code> file,
+	 * then this method may be empty.<br/>
 	 * Convenience helper methods {@link #addServlet(String, Class, String...)},
-	 * {@link #addFilter(String, Class, String...)} and {@link #addEndpoint(Class, String)} are
-	 * provided for the most common cases.
+	 * {@link #addFilter(String, Class, String...)}, {@link #addEndpoint(Class, String)} and
+	 * {@link #addEndpoint(Class, String, Configurator)} are provided for the most common cases.</p>
 	 */
 	protected abstract void configureServletsFiltersEndpoints() throws ServletException;
 	protected ServletContext ctx;
@@ -84,8 +87,8 @@ public abstract class GuiceServletContextListener implements ServletContextListe
 
 
 	/**
-	 * Adds async filter at the end of the chain (with <code>isMatchAfter==true</code>) for
-	 * dispatcher type <code>REQUEST</code> and injects its dependencies.
+	 * Adds async filter at the end of the chain (with <code>isMatchAfter==true</code> and
+	 * {@link DispatcherType#REQUEST}) and injects its dependencies.
 	 */
 	protected FilterRegistration.Dynamic addFilter(
 			String name, Class<? extends Filter> filterClass, String... urlPatterns)
@@ -102,15 +105,16 @@ public abstract class GuiceServletContextListener implements ServletContextListe
 
 
 	/**
-	 * Adds endpoint using {@link GuiceServerEndpointConfigurator}. Useful mostly for unannotated
-	 * endpoints extending <code>javax.websocket.Endpoint</code>.
+	 * Adds endpoint.
+	 * Useful mostly for unannotated endpoints extending {@link javax.websocket.Endpoint}.
 	 */
-	protected void addEndpoint(Class<?> endpointClass, String path) throws ServletException {
+	protected void addEndpoint(Class<?> endpointClass, String path, Configurator configurator)
+			throws ServletException {
 		try {
 			websocketContainer.addEndpoint(
 				ServerEndpointConfig.Builder
 					.create(endpointClass, path)
-					.configurator(new GuiceServerEndpointConfigurator())
+					.configurator(configurator)
 					.build());
 			log.info("registered endpoint " + endpointClass.getSimpleName());
 		} catch (DeploymentException e) {
@@ -118,12 +122,19 @@ public abstract class GuiceServletContextListener implements ServletContextListe
 		}
 	}
 
+	/**
+	 * Adds endpoint using {@link GuiceServerEndpointConfigurator} that injects the dependencies.
+	 * @see #addEndpoint(Class, String, Configurator)
+	 */
+	protected void addEndpoint(Class<?> endpointClass, String path) throws ServletException {
+		addEndpoint(endpointClass, path, new GuiceServerEndpointConfigurator());
+	}
+
 
 
 	@Override
 	public void contextInitialized(ServletContextEvent initializationEvent) {
 		try {
-			servletModule = new ServletModule();
 			LinkedList<Module> modules = configureInjections();
 			modules.add(servletModule);
 			INJECTOR = Guice.createInjector(modules);
@@ -142,8 +153,7 @@ public abstract class GuiceServletContextListener implements ServletContextListe
 
 			configureServletsFiltersEndpoints();
 		} catch (ServletException e) {
-			log.severe(e.toString());
-			e.printStackTrace();
+			log.log(Level.SEVERE, "could not start the server", e);
 			System.exit(1);
 		}
 	}
