48d0ecd4ed8b44037d912a66e40df4d476a9d89f
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-11-05 00:01:59 +0700

    jakarta conversion: fix javadoc links conversion

--- jakarta.sh
+++ jakarta.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Copyright (c) Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
 sed -E -e 's#(\t*).*<!--jakarta:(.*)-->#\1\2#' \
-	-e 's#(.*)javax(.*)<!--jakarta-->#\1jakarta-experimental\2#' \
+	-e 's#(.*)javax(.*)<!--jakarta-->#\1jakarta\2#' \
+	-e 's#(.*)javax(.*)<!--jakarta-experimental-->#\1jakarta-experimental\2#' \
 	<pom.xml >pom.jakarta.xml &&
 mv pom.jakarta.xml pom.xml &&
 
