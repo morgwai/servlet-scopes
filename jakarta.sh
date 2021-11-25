80ccf1ecf9a7cc932170d45b591a5863fcf5a861
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-11-25 17:12:09 +0700

    remove -experimental from the jakarta jar name
    Flavor jakarta still remains experimental though as stated in README.
    This is only to not make name too long.

--- jakarta.sh
+++ jakarta.sh
@@ -2,13 +2,11 @@
 # Copyright (c) Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
 sed -E -e 's#(\t*).*<!--jakarta:(.*)-->#\1\2#' \
 	-e 's#(.*)javax(.*)<!--jakarta-->#\1jakarta\2#' \
-	-e 's#(.*)javax(.*)<!--jakarta-experimental-->#\1jakarta-experimental\2#' \
 	<pom.xml >pom.jakarta.xml &&
 mv pom.jakarta.xml pom.xml &&
 
 sed -E -e 's#(\t*).*<!--jakarta:(.*)-->#\1\2#' \
 	-e 's#(.*)javax(.*)<!--jakarta-->#\1jakarta\2#' \
-	-e 's#(.*)javax(.*)<!--jakarta-experimental-->#\1jakarta-experimental\2#' \
 	<sample/pom.xml >sample/pom.jakarta.xml &&
 mv sample/pom.jakarta.xml sample/pom.xml &&
 
