6296ef292a067e66b229e90919bc7e500b8307ae
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2023-12-19 18:34:36 +0100

    jakarta.sh: don't break links in connection-proxy-tyrus tests

--- jakarta.sh
+++ jakarta.sh
@@ -25,7 +25,7 @@ sed -E -e 's#(\t*).*<!--jakarta:(.*)-->#\1\2#' \
 	<connection-proxy-tyrus/pom.xml >connection-proxy-tyrus/pom.jakarta.xml &&
 mv connection-proxy-tyrus/pom.jakarta.xml connection-proxy-tyrus/pom.xml &&
 
-for folder in src connection-proxy-tyrus/src; do
+for folder in src connection-proxy-tyrus/src/main connection-proxy-tyrus/src/test/java/pl/morgwai/base/servlet/guice/scopes/connectionproxy/tyrus; do
   find ${folder} -name '*.java' | while read file; do
     sed -e 's#javax.servlet#jakarta.servlet#g' \
       -e 's#javax.websocket#jakarta.websocket#g' \
