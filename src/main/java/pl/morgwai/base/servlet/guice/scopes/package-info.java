32f0d48df86c5d4dfbcdf8d8f0278e9baa80294a
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2025-04-14 02:41:03 +0200

    tiny javadoc updates

--- src/main/java/pl/morgwai/base/servlet/guice/scopes/package-info.java
+++ src/main/java/pl/morgwai/base/servlet/guice/scopes/package-info.java
@@ -1,3 +1,6 @@
 // Copyright 2021 Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
-/** The main package for servlet and websocket scopes. */
+/**
+ * The main package for {@link javax.servlet.Servlet} and websocket
+ * {@link com.google.inject.Scope}s.
+ */
 package pl.morgwai.base.servlet.guice.scopes;
